c4d9daa25179cb5036f862769289e7296f4d8386
z h <zhanif02@gmail.com>
2024-07-17 17:40:40 +0100

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,23 @@ const Footer = () => {
             <span className="text-[#70757a] text-[15px] leading-none">
                 United Kingdom
             </span>
-
         </div>
-
+        <div className="flex flex-col md:flex-row justify-between py-3 md:py-0 md:px-[15px">
+            <div className="flex justify-items-center">
+                {quickLinks.map((menu, index) =>(
+                    <span key={index} className="text-[#70757a] text-[12px] md:text-[14px] leading-none p-[10px] md:p[15px]">
+                        {menu}
+                    </span>
+                ))}
+            </div>
+            <div className="flex justify-center">
+                {settingMenu.map((menu, index) =>(
+                    <span key={index} className="text-[#70757a] text-[12px] md:text-[14px] leading-none p-[10px] md:p[15px]">
+                        {menu}
+                    </span>
+                ))}
+            </div>
+        </div>
     </div>;
 };
 
