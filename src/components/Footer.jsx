f5a5945d897a928e71bea52ed4c803ab15251296
z h <zhanif02@gmail.com>
2024-07-17 17:32:29 +0100

    footer country added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
 import { quickLinks, settingMenu } from "../utils/Constants";
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return <div className="bg-[#f2f2f2]">
+        <div className="flex py-[15px] px-[15px] md:px-[30px] border-b border-[#dadce0]">
+            <span className="text-[#70757a] text-[15px] leading-none">
+                United Kingdom
+            </span>
+
+        </div>
+
+    </div>;
 };
 
 export default Footer;
