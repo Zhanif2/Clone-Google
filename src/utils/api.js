9d99c537b0ab756467d84e0ae657308c6ea98699
z h <zhanif02@gmail.com>
2024-07-18 20:11:05 +0100

    secret hidden

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const BASE_URL = "https://www.googleapis.com/customsearch/v1";
 
 const params = {
-  key: "AIzaSyBu_nRmQjTEllTbVPNUbR1MhevvS6w0-dI",
-  cx: "c7b3da0a8446b471e",
+ // key: "AIzaSyBu_nRmQjTEllTbVPNUbR1MhevvS6w0-dI",
+  //cx: "c7b3da0a8446b471e",
 };
 
 export const fetchDataFromApi = async (payload) => {
