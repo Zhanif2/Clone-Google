108ccd95b51199c4032164f244dffc921cb50ae0
z h <zhanif02@gmail.com>
2024-07-19 18:54:49 +0100

    generated img search

--- src/components/SearchedImageItemTemplate.jsx
+++ src/components/SearchedImageItemTemplate.jsx
@@ -1,5 +1,9 @@
-const SearchedImageItemTemplate = () => {
-    return <div>Searched Image Template</div>;
+const SearchedImageItemTemplate = ({data}) => {
+    return <div className="group flex flex-col py-3 cursor-pointer" onClick={() => window.open(data.image.contextLink)}>
+        <div className="rounded-xl overflow-hidden bg-black/[0.03] h-[100px] md:h-[120px] lg:-h[140px] group-hover:shadow-c">
+        <img src={data.link} className="h-full w-full object-contain" />
+        </div>
+    </div>
 };
 
 export default SearchedImageItemTemplate;
